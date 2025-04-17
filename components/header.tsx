0aaafd04f67f3788540c89cd320784632a67961f
Rajdeep Singh <radewala.2@gmail.com>
2025-04-17 22:42:29 +0530

    header component

---
+++ components/header.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+
+import { Dock, DockIcon } from "@/components/magicui/dock";
+import { Home, Settings, Search, Contact } from "lucide-react";
+
+export function Header() {
+  return (
+    <Dock direction="middle" iconMagnification={60} iconDistance={100}>
+      <DockIcon>
+        <Home className="size-5" />
+      </DockIcon>
+      <DockIcon>
+        <Settings className="size-5" />
+      </DockIcon>
+      <DockIcon>
+        <Contact className="size-5" />
+      </DockIcon>
+      <DockIcon>
+        <Search className="size-5" />
+      </DockIcon>
+    </Dock>
+  );
+}
