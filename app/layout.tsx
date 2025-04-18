c18eeb896f618226bacd87b273c66a05309fffa4
Rajdeep Singh <radewala.2@gmail.com>
2025-04-18 17:32:11 +0530

    wrap with ThemeProvider using next themes package

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { DotPattern } from "@/components/magicui/dot-pattern";
 import { Header } from "@/components/header";
 import { cn } from "@/lib/utils";
 import { Meteors } from "@/components/magicui/meteors";
-
+import { ThemeProvider } from "next-themes";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,18 +31,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        <div className="relative flex h-screen  w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background">
-          <Meteors />
+        <ThemeProvider>
+          <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg ">
+            <Meteors />
 
-          <DotPattern
-            className={cn(
-              "[mask-image:radial-gradient(1200px_circle_at_center,white,transparent)]",
-            )}
-          />
+            <DotPattern
+              className={cn(
+                "[mask-image:radial-gradient(1200px_circle_at_center,white,transparent)]",
+              )}
+            />
 
-          {children}
-          <Header />
-        </div>
+            {children}
+            <Header />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
